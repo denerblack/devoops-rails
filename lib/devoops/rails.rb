0e08bc70efca1360bbd0466738ef9fc9ed9cf6cd
Dener W P do Carmo <dener.wilian@gmail.com>
2014-06-03 23:55:21 -0400

    ajustado assets

--- lib/devoops/rails.rb
+++ lib/devoops/rails.rb
@@ -2,6 +2,11 @@ require "devoops/rails/version"
 
 module Devoops
   module Rails
+		class Engine < ::Rails::Engine
+			initializer "devoops-rails.assets.precompile" do |app|
+				app.config.assets.precompile += %w( css/style.css )
+			end
+		end
     # Your code goes here...
   end
 end
