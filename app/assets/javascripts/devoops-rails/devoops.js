f34ba0773154cdb40e6c99b2e1f09b4ced320674
Dener W P do Carmo <dener.wilian@gmail.com>
2016-03-21 10:57:19 -0400

    Removed ajax call in js

--- app/assets/javascripts/devoops-rails/devoops.js
+++ app/assets/javascripts/devoops-rails/devoops.js
@@ -2065,11 +2065,7 @@ $(document).ready(function () {
     $('div#main').toggleClass('sidebar-show');
     setTimeout(MessagesMenuWidth, 250);
   });
-  var ajax_url = location.hash.replace(/^#/, '');
-  if (ajax_url.length < 1) {
-    ajax_url = 'ajax/dashboard.html';
-  }
-  LoadAjaxContent(ajax_url);
+
   $('.main-menu').on('click', 'a', function (e) {
     var parents = $(this).parents('li');
     var li = $(this).closest('li.dropdown');
